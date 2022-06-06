7eef6d68ec5bdbf9a3bf922619560c13d72c4c82
John Tuck <j.tuck1336@gmail.com>
2022-06-06 11:18:51 -0400

    Weekly Challenges 2

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,109 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return Char.IsLetter(c);
         }
-
+        
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            //or use this code
+            //return vals.Length % 1 == 0;
+            if (vals.Length % 2 == 0)
+                return true;
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            //return (number % 2 == 0);
+            if (number % 2 == 0)
+                return true;
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            //return (number % 2 != 0)
+            if (num % 2 != 0)
+                return true;
+            return false;
+
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            //return numbers == null || numbers.Count() == 0
+            // ? 0 : numbers.Min() + numbers.Max();
+            if (numbers == null || numbers.Count() == 0) { return 0; }
+            var small = double.MaxValue;
+            var large = double.MinValue;
+        for (int i = 0; i < numbers.Count(); i++)
+            {
+                if (numbers.ElementAt(i) < small)
+                {
+                    small = numbers.ElementAt(i);
+                }
+                if (numbers.ElementAt(i) > large)
+                {
+                    large = numbers.ElementAt(i);
+                }
+            }
+            return small + large;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return str1.Length < str2.Length ? str1.Length : str2.Length;
+            /*if (str1.Length < str2.Length)
+                return str1.Length;
+            return str2.Length;*/
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers == null ? 0 : numbers.Sum();
+            if (numbers == null)
+            { return 0; }
+            return numbers.Sum();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            //return numbers == null ? 0 : numbers.Where(num => num % 2 == 0).Sum();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            var sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }    
+                return sum;
+        
+            
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { return false; }
+            // return numbers == null ? false : numbers.Sum() % 2 != 0;
+            var sum = 0;
+           for (int i = 0; i < numbers.Count(); i++)
+            {
+                sum += numbers[i];
+              //sum = sum + numbers[i];
+            }
+            return sum % 2 != 0;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            return number <0 ? 0 : number / 2;
         }
     }
 }
