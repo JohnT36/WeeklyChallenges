fc84c78c8822493f33798739703108c39c9d14c1
John Tuck <j.tuck1336@gmail.com>
2022-06-07 15:13:09 -0400

    Assignment Completed *3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -6,94 +6,108 @@ namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
+
+
         public bool ArrayContainsAFalse(bool[] vals)
         {
-#pragma warning disable CS0162 // Unreachable code detected
+            
             for (int i = 0; i < vals.Length; i++)
             {
                 if (vals[i] == false)
-                {
                     return true;
-                }
-                
-            } return false;
-#pragma warning restore CS0162 // Unreachable code detected
-
+            }
+            return false;
         }
+
+
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
             if (numbers == null || numbers.Count() == 0)
-            { return false; }
-
-            var sum = numbers.Sum();
+                return false;
+            
+                var sum = numbers.Sum();
             return sum % 2 != 0;
 
+
+            
         }
+
+
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            var isLower = false;
-            var isUpper = false;
-            var isNumber = false;
+            var upper = false;
+            var lower = false;
+            var number = false;
 
-            for (var i = 0; i < password.Length; i++)
+            for (int i = 0; i < password.Length; i++)
             {
-                if (char.IsLower(password[i]))
-                { isLower = true; }
-                if (char.IsUpper(password[i]))
-                { isUpper = true; }
-                if (char.IsNumber(password[i]))
-                { isNumber = true; }
-            }
-            if (isLower && isUpper && isNumber )
-                { return true; }
-             return false;
+                if (Char.IsLower(password[i]))
+                    lower = true;
+                if (Char.IsUpper(password[i]))
+                    upper = true;
+                if (Char.IsNumber(password[i]))
+                    number = true;
+             }
+            
+            if (upper && lower && number) return true;
+            return false;
+
         }
+
+
 
         public char GetFirstLetterOfString(string val)
         {
             return val.First();
-            // return val[0];
-            //return val.First();
         }
+
+
 
         public char GetLastLetterOfString(string val)
         {
-            return val[^1];
-            //return val.Last();
-            //return val[val.Length - 1]);
+            return val[val.Length - 1];
         }
+
+
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
             if (divisor == 0)
-            { return 0; }
+                return 0;
             return dividend / divisor;
         }
+
+
 
         public int LastMinusFirst(int[] nums)
         {
             return nums[^1] - nums[0];
         }
 
+
+
         public int[] GetOddsBelow100()
         {
-            var oddSum = new List<int>();
-            
+            var odds = new List<int>();
+
             for (int i = 0; i < 100; i++)
             {
-                if (i % 2 != 0)
-                {
-                    oddSum.Add(i);
-                }
+                if (i % 2 != 0) odds.Add(i);
             }
-            return oddSum.ToArray();
+            return odds.ToArray();
         }
+
+
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
             for (int i = 0; i < words.Length; i++)
+            {
                 words[i] = words[i].ToUpper();
+            }
         }
+
+
     }
 }
