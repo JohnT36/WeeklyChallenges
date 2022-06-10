b09c89e061106d407341b2b8a3fbdc9942804d44
John Tuck <j.tuck1336@gmail.com>
2022-06-09 23:29:03 -0400

    Weekly Challenges 4 Done. What a Dooosey

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,52 +1,120 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet04
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            /*var sum = 0;
+            foreach (var number in numbers)
+            {
+                if (number % 2 == 0)
+                    sum += number;
+                if (number % 2 != 0)
+                    sum -= number;
+                
+            } return sum;*/
+            return numbers.Where(x=>x % 2 == 0).Sum() - numbers.Where(x=>x % 2 != 0).Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var list = new List<string>() { str1, str2, str3, str4 };
+            return list.Min(x => x.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var list = new List<int>() { number1, number2, number3, number4 };
+            return list.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+
+            if (sideLength1 + sideLength2 > sideLength3
+            && sideLength1 + sideLength3 > sideLength2
+            && sideLength2 + sideLength3 > sideLength1)
+            { return true; }
+            return false;
+            
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            var isNumber = double.TryParse(input, out var number);
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var amount = objs.Length;
+            var majority = (amount / 2) + 1;
+
+            var count = 0;
+
+            foreach ( var obj in objs )
+            {  if ( obj == null )
+                { count++; }                  
+                     
+            }
+            if (count >= majority)
+            { return true; }
+            return false;
+            
+
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+         //var sum = 0;
+         //var count = 0;
+         //if ( numbers == null)
+         //    { return 0; }
+         //
+         //foreach (var number in numbers )
+         //{
+         //    if (number % 2 == 0)
+         //    { 
+         //        sum += number;
+         //        count++; 
+         //    }
+         //
+         //}
+         //if (count > 0)
+         //{
+         //    return sum / count;
+         //}
+         //else
+         //{
+         //    return 0;
+         //}
+            
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+            var n = numbers.Where(x => x % 2 == 0);
+            if (n.Count() == 0)
+                { return 0; }
+            return n.Average();
+            
+            
+            
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+            { throw new ArgumentOutOfRangeException("number"); }
+            if (number > 0)
+            { return 1; }
+            return number * Factorial(number - 1);
         }
     }
 }
